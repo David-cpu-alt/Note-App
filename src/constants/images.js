3ec6a4b02f0b9ba2224148a1afba5c3ef4f4fe01
David-cpu-alt <davidoladejo2020@gmail.com>
2024-03-21 13:14:34 +0100

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,26 @@
+
+const news = require("../../assets/images/news.png")
+const circle = require("../../assets/images/circle.png")
+const stella = require("../../assets/images/stella.jpg")
+const oke = require("../../assets/images/oke.jpg")
+const tunde = require("../../assets/images/tunde.jpg")
+const bitcoin = require("../../assets/images/bitcoin.jpg")
+const goggle = require("../../assets/images/goggle.jpg")
+const american = require("../../assets/images/american.jpeg")
+const smile = require("../../assets/images/smile.jpeg")
+const lit = require("../../assets/images/lit.jpeg")
+const hema = require("../../assets/images/hema.jpeg")
+
+export default {
+    news,
+    circle,
+    stella,
+    oke,
+    tunde,
+    bitcoin,
+    goggle,
+    american,
+    smile,
+    lit,
+    hema
+}
